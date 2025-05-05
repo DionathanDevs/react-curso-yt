a8ccc3bcd3bc479800caa57775a4abf297adabb1
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-05 13:59:30 -0300

    Ajustes em deletar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
+import { useState } from "react"
 import AddTask from "./components/AddTasks"
 import Tasks from "./components/Tasks"
 
 
 function App(){
 
+const [tasks, setTasks] = useState([
+  {
+  id: 1,
+  title: "Estudar programação",
+  description: "Estudar programação para se tornar um dev full stack",
+  isCompleted: false
+  },
+  {
+  id: 2,
+  title: "Estudar Inglês",
+  description: "Estudar Inglês para se tornar um dev full stack",
+  isCompleted: false
+  }
+])
 
+function onTaskClick(taskId){
+  const newTasks = tasks.map(task => {
+
+
+    if(task.id == taskId){
+      return{
+        ...task, isCompleted: !task.isCompleted
+      }
+    } 
+
+    return task;
+
+
+  
+  })
+  setTasks(newTasks);
+}
+
+
+function onDeleteTaskClick(taskId){
+  const newTasks = tasks.filter(task => task.id != taskId)
+  setTasks(newTasks);
+}
 
   return(
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+      <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
       <AddTask />
-      <Tasks />
+      <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+      </div>
     </div>
   )
 
--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,8 +1,12 @@
 function AddTask(){
 
     return (
-        <h1>Add Task</h1>
-    )
+    <div className="bg-slate-200 rounded shadow space-y-4 p-6 flex flex-col ">
+        <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Digite o título da tarefa" />
+        <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Digite a descrição da tarefa" />
+        <button className="bg-slate-500 text-white px-4 py-2 font-medium rounded-md">Adicionar</button>
+    </div>
+   )
 }
 
 export default AddTask
